2b75d2837afd19ef04acfa82fafc4af5ffce143a
ramoncord <ramonestii@hotmail.com>
2018-10-08 23:28:38 -0300

    subject rspec

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,22 +1,23 @@
 #teste
 require 'calculator'
 describe Calculator do
+	#subject(:calc){describe_class.new()}
+	#a linha a cima serve para quando nao quer usar subject'
+	#mas quer definir uma palavra, usa ela no lugar.
+	#exemplo: result = calc.sum(5,7)
 	context '#sum' do
 		it 'with positive numbers' do
-			calc = Calculator.new
-			result = calc.sum(5,7)
+			result = subject.sum(5,7)
 			expect(result).to eq(12)
 		end
 
 		it 'with negative and positive numbers' do
-			calc = Calculator.new
-			result = calc.sum(-5,7)
+			result = subject.sum(-5,7)
 			expect(result).to eq(2)
 		end
 
 		it 'with negative numbers' do
-			calc = Calculator.new
-			result = calc.sum(-5,-7)
+			result = subject.sum(-5,-7)
 			expect(result).to eq(-12)
 		end
 
