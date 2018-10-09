275b228537bad28ed50b1eb985193b3d043b1358
ramoncord <ramonestii@hotmail.com>
2018-10-08 23:14:10 -0300

    context and .rspec

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,16 +1,24 @@
 #teste
 require 'calculator'
 describe Calculator do
-	it 'sum method for 2 numbers' do
-		calc = Calculator.new
-		result = calc.sum(5,7)
-		expect(result).to eq(12)
-	end
+	context '#sum' do
+		it 'with positive numbers' do
+			calc = Calculator.new
+			result = calc.sum(5,7)
+			expect(result).to eq(12)
+		end
 
-	it 'sum method for 2 numbers with negative numbers' do
-		calc = Calculator.new
-		result = calc.sum(-5,7)
-		expect(result).to eq(2)
-	end
+		it 'with negative and positive numbers' do
+			calc = Calculator.new
+			result = calc.sum(-5,7)
+			expect(result).to eq(2)
+		end
 
+		it 'with negative numbers' do
+			calc = Calculator.new
+			result = calc.sum(-5,-7)
+			expect(result).to eq(-12)
+		end
+
+	end
 end
